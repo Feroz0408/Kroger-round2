ba5b4c7133242d8ea555e6cf68b51893b154bd74
FEROZ SHAIK <feroz.ksr39@example.com>
2019-12-01 18:21:03 -0500

    styling changes 2

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Category from './components/Category';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Category />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
